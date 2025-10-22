197733754c1bebd0739152367ab03f1a3200cb82
Yanpingok <Yanpingok@users.noreply.github.com>
2025-10-22 06:01:31 +0000

    deploy: e37fd383387624b4f98a394d7311079f7588ad7e

---
+++ trait.impl/sui_types/object/option_visitor/trait.OptionVisitorError.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sui_rpc_resolver",[["impl OptionVisitorError for <a class=\"enum\" href=\"sui_rpc_resolver/json_visitor/enum.Error.html\" title=\"enum sui_rpc_resolver::json_visitor::Error\">Error</a>"]]],["sui_types",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[190,17]}
